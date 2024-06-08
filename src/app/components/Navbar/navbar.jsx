9010c6c2a7101c51fb0ab156d55d62d36a3ae476
afradenburg <andres.burg@hotmail.com>
2024-06-07 22:15:05 -0500

    update layout

--- src/app/components/Navbar/navbar.jsx
+++ src/app/components/Navbar/navbar.jsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import './navbar.css';
 
-const Navbar = () => {
+export const Navbar = () => {
   return (
     <div className="navBar">
         <Link href={"https://drive.google.com/file/d/1I6dCJQMjLm1qXgIsg6yc8aSq-XTMJ3wn/view?usp=drive_link"} style={{textDecoration: 'none'}} download>
@@ -13,4 +13,3 @@ const Navbar = () => {
     </div>
   )
 }
-export default Navbar;
