340289296e81dd49b3717005cc5ef0ef84673863
afradenburg <andres.burg@hotmail.com>
2024-06-07 22:05:25 -0500

    cambios

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google'
 import './index.css'
-import Navbar from "../app/components/navbar/navbar"
+import Navbar from "../app/components/navbar/Navbar"
 
 const inter = Inter({ subsets: ['latin'] })
 
