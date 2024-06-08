d4e2f9a19af40507d9e86a3f258c3ce0058d9c7f
afradenburg <andres.burg@hotmail.com>
2024-06-07 21:40:38 -0500

    update cv

--- src/app/components/Navbar/navbar.jsx
+++ src/app/components/Navbar/navbar.jsx
@@ -5,7 +5,7 @@ import './navbar.css';
 const Navbar = () => {
   return (
     <div className="navBar">
-        <Link href={"https://drive.google.com/file/d/1uSOZ7asx-ZWn-hSaX87n1RyI_Q9i9w4G/view?usp=drive_link"} style={{textDecoration: 'none'}} download>
+        <Link href={"https://drive.google.com/file/d/1I6dCJQMjLm1qXgIsg6yc8aSq-XTMJ3wn/view?usp=drive_link"} style={{textDecoration: 'none'}} download>
         <p style={{marginRight: "20px"}}>
             CV
         </p>
